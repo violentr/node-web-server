95c2e19e168c7406572bb46e7bb4f59a7258952c
Deniss <den.b@itservicesgroup.co.uk>
2018-01-10 21:33:22 +0000

    Add handlebars helpers getCurrentYear, screamIt
    Clean up unnecessary code

--- server.js
+++ server.js
@@ -5,17 +5,23 @@ hbs.registerPartials(__dirname + '/views/partials/');
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
 
+hbs.registerHelper('getCurrentYear', () =>{
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) =>{
+  return text.toUpperCase();
+})
+
 app.get('/', (req, res) => {
   res.render('home.hbs', {
-    pageTitle: 'Welcome to my website',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'Welcome to my website'
   })
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
