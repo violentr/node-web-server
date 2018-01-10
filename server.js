06f6200f3796d41c911ca3d89d0c560ea3c65178
Deniss <den.b@itservicesgroup.co.uk>
2018-01-10 22:02:06 +0000

    Add Express midleware to log all requests in server.log file

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials/');
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log file')
+    }
+  })
+  next();
+})
 
 hbs.registerHelper('getCurrentYear', () =>{
   return new Date().getFullYear();
