c74ca596fc5e15293c3bdf8e2ead46a7406ab8a0
Deniss <den.b@itservicesgroup.co.uk>
2018-01-10 22:10:15 +0000

    Add Introduce maintenance option with express midleware
    Order is very important, when visit any page
    will show maintenance template, there is no
    next() word so it will only render maintenance
    template and nothing else.

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials/');
 app.set('view engine', 'hbs')
-app.use(express.static(__dirname + '/public'))
 
 app.use((req, res, next) => {
   var now = new Date().toString();
@@ -18,6 +17,12 @@ app.use((req, res, next) => {
   })
   next();
 })
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs')
+})
+
+app.use(express.static(__dirname + '/public'))
 
 hbs.registerHelper('getCurrentYear', () =>{
   return new Date().getFullYear();
