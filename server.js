4f3d38af1a82c4a607852dadc5054c85df419b80
Deniss <den.b@itservicesgroup.co.uk>
2018-01-10 20:53:19 +0000

    Add About page as handlebars template
    Inject few vars into page,
    simple text and Date as current year

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
+const hbs = require('hbs');
 var app = express();
+app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
   res.send('Hello Express')
 });
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
 
 app.listen(3000);
 
