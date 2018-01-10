ff9f9b40f3bda0e8a71f4406c743677b9bf8355b
Deniss <den.b@itservicesgroup.co.uk>
2018-01-10 21:09:37 +0000

    Register partials, create footer partial
    Inject footer partial into hbs template

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const hbs = require('hbs');
 var app = express();
+hbs.registerPartials(__dirname + '/views/partials/');
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  res.render('home.hbs', {
+    pageTitle: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
+  })
 });
 
 app.get('/about', (req, res) => {
