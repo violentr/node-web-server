0dc8d143823381e34fe5b20751de0d723e8c9c89
Deniss <den.b@itservicesgroup.co.uk>
2018-01-11 23:26:47 +0000

    Add Spec, testing async function with callback

--- tests/utils.js
+++ tests/utils.js
@@ -1,1 +1,7 @@
 module.exports.add = (a, b) => a + b
+
+module.exports.asyncAdd = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a + b);
+  }, 1000)
+}
--- tests/utils.test.js
+++ tests/utils.test.js
@@ -6,3 +6,10 @@ it('should add two numbers', () =>{
   expect(res).toBe(44)
   expect(typeof res).toBe('number');
 })
+
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7);
+    done();
+  })
+})
