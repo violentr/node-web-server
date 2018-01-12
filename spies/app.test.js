08bf101484bb32a5d5643ee8c8af34987e5ebbfd
Deniss <den.b@itservicesgroup.co.uk>
2018-01-12 22:14:59 +0000

    Add Simulate calling #handleSignup with spy
    Use 'npm rewire' to make call to spy what emulates
    call to function(#handleSignup) with params you need.
    This is how you may test that function was
    called

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,11 +1,26 @@
+// https://github.com/jhnns/rewire
 const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
 
 
 describe('App', () => {
+  var db = {
+    saveUser: expect.createSpy()
+  }
+  app.__set__('db', db)
 
   it('should call the spy correctly', () => {
     var spy = expect.createSpy();
     spy('Deb', 34);
     expect(spy).toHaveBeenCalledWith('Deb', 34);
+  });
+
+  it('should call saveUser with user object', () => {
+    var email = 'example@email.com'
+    var password = '1234c';
+    app.handleSignup(email, password)
+    expect(db.saveUser).toHaveBeenCalledWith({email, password})
   })
 })
